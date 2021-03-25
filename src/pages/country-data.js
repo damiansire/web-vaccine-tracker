8cffdd5def73eb2f35d10ca9eebf42ce6f73c66a
Damian Sire <32693792+damiansire@users.noreply.github.com>
2021-03-25 01:55:09 -0300

    Fixs react dependency error

--- src/pages/country-data.js
+++ src/pages/country-data.js
@@ -8,6 +8,8 @@ import TextField from "@material-ui/core/TextField";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import CheckBoxOutlineBlankIcon from "@material-ui/icons/CheckBoxOutlineBlank";
 import CheckBoxIcon from "@material-ui/icons/CheckBox";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
 
 const CountryData = () => {
   const [loadCountryData, setLoadCountryData] = useState(false);
@@ -26,85 +28,85 @@ const CountryData = () => {
   const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
   const checkedIcon = <CheckBoxIcon fontSize="small" />;
 
-  //Create a cache
-  const countriesDataCached = {};
+  useEffect(() => {
+    //Create a cache
+    const countriesDataCached = {};
 
-  const saveCountryDataInCached = (countryId, countryVaccinesData) => {
-    countriesDataCached[countryId] = {
-      total_vaccines: countryVaccinesData,
-      countryId: countryId,
+    const saveCountryDataInCached = (countryId, countryVaccinesData) => {
+      countriesDataCached[countryId] = {
+        total_vaccines: countryVaccinesData,
+        countryId: countryId,
+      };
     };
-  };
 
-  const getSelectedCountriesDataForGraph = async () => {
-    //Get not cached element
-    let notCachedCountries = selectedCountries.filter(
-      (country) => !countriesDataCached.hasOwnProperty(country)
-    );
-    if (notCachedCountries.length > 0) {
-      //Get countries data not cached and cached it
-      let contriesData = await Promise.all(
-        notCachedCountries.map(async (country) => {
-          let countryResponse = await fetch(
-            getVaccineApiEndpointForCountry(country)
+    const getSelectedCountriesDataForGraph = async () => {
+      //Get not cached element
+      let notCachedCountries = selectedCountries.filter(
+        (country) => !countriesDataCached.hasOwnProperty(country)
+      );
+      if (notCachedCountries.length > 0) {
+        //Get countries data not cached and cached it
+        let contriesData = await Promise.all(
+          notCachedCountries.map(async (country) => {
+            let countryResponse = await fetch(
+              getVaccineApiEndpointForCountry(country)
+            );
+            return countryResponse.json();
+          })
+        );
+
+        contriesData.forEach((countryData) => {
+          saveCountryDataInCached(
+            countryData["countryId"],
+            countryData["vaccine"]
           );
-          return countryResponse.json();
-        })
+        });
+      }
+      return selectedCountries.map((countryName) => {
+        return {
+          name: countriesDataCached[countryName]["countryId"],
+          data: countriesDataCached[countryName]["total_vaccines"],
+        };
+      });
+    };
+
+    const getGraphObj = (dates, countriesDataArray) => {
+      //Dates es un array de fechas en formato string
+      //CountriesDataArray es de la forma [ { name: countryId, data: vaccineCountry } ]
+      return {
+        options: {
+          chart: {
+            id: "basic-bar",
+          },
+          xaxis: {
+            categories: dates,
+          },
+        },
+        series: countriesDataArray,
+      };
+    };
+
+    const setSelectedCountriesData = async () => {
+      let selectedCountryData = await getSelectedCountriesDataForGraph();
+      //TODO: Controlar caso en el que no hay paises seleccionados
+      if (selectedCountryData.length === 0) {
+        return;
+      }
+      let dateForGraph = selectedCountryData[0]["data"].map(
+        (country) => country["date"]
       );
 
-      contriesData.forEach((countryData) => {
-        saveCountryDataInCached(
-          countryData["countryId"],
-          countryData["vaccine"]
-        );
+      let vaccineCountries = selectedCountryData.map((country) => {
+        return {
+          name: country.name,
+          data: country.data.map((dataPoint) => dataPoint["total_vaccines"]),
+        };
       });
-    }
-    return selectedCountries.map((countryName) => {
-      return {
-        name: countriesDataCached[countryName]["countryId"],
-        data: countriesDataCached[countryName]["total_vaccines"],
-      };
-    });
-  };
-
-  const getGraphObj = (dates, countriesDataArray) => {
-    //Dates es un array de fechas en formato string
-    //CountriesDataArray es de la forma [ { name: countryId, data: vaccineCountry } ]
-    return {
-      options: {
-        chart: {
-          id: "basic-bar",
-        },
-        xaxis: {
-          categories: dates,
-        },
-      },
-      series: countriesDataArray,
+      let newData = getGraphObj(dateForGraph, vaccineCountries);
+      setGraphState(newData);
+      setLoadCountryData(true);
     };
-  };
 
-  const setSelectedCountriesData = async () => {
-    let selectedCountryData = await getSelectedCountriesDataForGraph();
-    //TODO: Controlar caso en el que no hay paises seleccionados
-    if (selectedCountryData.length === 0) {
-      return;
-    }
-    let dateForGraph = selectedCountryData[0]["data"].map(
-      (country) => country["date"]
-    );
-
-    let vaccineCountries = selectedCountryData.map((country) => {
-      return {
-        name: country.name,
-        data: country.data.map((dataPoint) => dataPoint["total_vaccines"]),
-      };
-    });
-    let newData = getGraphObj(dateForGraph, vaccineCountries);
-    setGraphState(newData);
-    setLoadCountryData(true);
-  };
-
-  useEffect(() => {
     setSelectedCountriesData();
   }, [selectedCountries]);
 
@@ -112,82 +114,101 @@ const CountryData = () => {
     <div className="app">
       {loadCountryData && (
         <>
-          <div className="row">
-            <Button
-              variant="outlined"
-              size="large"
-              color="primary"
-              onClick={() => {
-                setGraphType("line");
-              }}
+          <Grid container style={{ padding: 20 }}>
+            <Grid
+              container
+              direction="row"
+              item
+              xs={8}
+              justify="space-evenly"
+              alignItems="center"
             >
-              Graficos de Lineas
-            </Button>
-            <Button
-              variant="outlined"
-              size="large"
-              color="primary"
-              onClick={() => {
-                setGraphType("bar");
-              }}
-            >
-              Graficos de Barra
-            </Button>
-            <Autocomplete
-              onChange={(event, newInputValue) => {
-                setSelectedCountries(
-                  newInputValue.map((element) => element["countryId"])
-                );
-              }}
-              multiple
-              id="checkboxes-tags-demo"
-              options={allCountries}
-              disableCloseOnSelect
-              getOptionLabel={(option) => option.countryId}
-              renderOption={(option, { selected }) => (
-                <React.Fragment>
-                  <Checkbox
-                    icon={icon}
-                    checkedIcon={checkedIcon}
-                    style={{ marginRight: 8 }}
-                    checked={selected}
-                  />
-                  {option.countryId}
-                </React.Fragment>
-              )}
-              style={{ width: 500 }}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
+              <div>
+                <Button
                   variant="outlined"
-                  label="Checkboxes"
-                  placeholder="Favorites"
-                />
-              )}
-            />
-          </div>
-          <div className="row">
-            {graphType === "bar" && (
-              <div className="mixed-chart">
-                <Chart
-                  options={state.options}
-                  series={state.series}
-                  type={graphType}
-                  width="1000"
-                />
+                  size="large"
+                  color="primary"
+                  onClick={() => {
+                    setGraphType("line");
+                  }}
+                >
+                  Graficos de Lineas
+                </Button>
               </div>
-            )}
-            {graphType === "line" && (
-              <div className="mixed-chart">
-                <Chart
-                  options={state.options}
-                  series={state.series}
-                  type={graphType}
-                  width="1000"
-                />
+              <div>
+                <Button
+                  variant="outlined"
+                  size="large"
+                  color="primary"
+                  onClick={() => {
+                    setGraphType("bar");
+                  }}
+                >
+                  Graficos de Barra
+                </Button>
               </div>
-            )}
-          </div>
+            </Grid>
+            <Grid xs={4} container alignItems="center" justify="space-evenly">
+              <Grid item>
+                <Autocomplete
+                  onChange={(event, newInputValue) => {
+                    setSelectedCountries(
+                      newInputValue.map((element) => element["countryId"])
+                    );
+                  }}
+                  multiple
+                  id="checkboxes-tags-demo"
+                  options={allCountries}
+                  disableCloseOnSelect
+                  getOptionLabel={(option) => option.countryId}
+                  renderOption={(option, { selected }) => (
+                    <>
+                      <Checkbox
+                        icon={icon}
+                        checkedIcon={checkedIcon}
+                        style={{ marginRight: 8 }}
+                        checked={selected}
+                      />
+                      {option.countryId}
+                    </>
+                  )}
+                  style={{ width: 400 }}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      variant="outlined"
+                      label="Select countries"
+                      placeholder="Favorites"
+                    />
+                  )}
+                />
+              </Grid>
+            </Grid>
+            <Grid item xs={8}>
+              <div className="row">
+                {graphType === "bar" && (
+                  <div className="mixed-chart">
+                    <Chart
+                      options={state.options}
+                      series={state.series}
+                      type={graphType}
+                      width="1000"
+                    />
+                  </div>
+                )}
+                {graphType === "line" && (
+                  <div className="mixed-chart">
+                    <Chart
+                      options={state.options}
+                      series={state.series}
+                      type={graphType}
+                      width="1000"
+                    />
+                  </div>
+                )}
+              </div>
+            </Grid>
+          </Grid>
         </>
       )}
     </div>
