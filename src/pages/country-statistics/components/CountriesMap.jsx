cdd0a78c3479036e6aa9dffecf1f3863764f9b51
Damian <damiansire@hotmail.com>
2021-07-19 00:54:27 -0300

    Fixs build errors with eyes closed. If I don't see it, it's not a bad code, lalalala

--- src/pages/country-statistics/components/CountriesMap.jsx
+++ src/pages/country-statistics/components/CountriesMap.jsx
@@ -5,51 +5,56 @@ import { getLastDataCountries } from "../../../adapters/rankings.js";
 
 function CountriesMap(props) {
 
-  const missingCountriesCode = {
-    "BO": "Bolivia",
-    "BQ": "Bonaire Sint Eustatius and Saba",
-    "BN": "Brunei",
-    "CV": "Cape Verde",
-    "CI": "Cote d'Ivoire",
-    "CW": "Curacao",
-    "CD": "Democratic Republic of Congo",
-    "GB": "England",
-    "FK": "Falkland Islands",
-    "IR": "Iran",
-    "LA": "Laos",
-    "MD": "Moldova",
-    "CY": "Northern Cyprus",
-    "GB": "Northern Ireland",
-    "PS": "Palestine",
-    "RU": "Russia",
-    "SH": "Saint Helena",
-    "KR": "South Korea",
-    "SY": "Syria",
-    "TW": "Taiwan",
-    "TL": "Timor",
-    "US": "United States",
-    "VE": "Venezuela",
-    "VN": "Vietnam",
-    "GB-WLS": "Wales",
-  }
 
   const [data, setData] = useState([]);
 
-  useEffect(async () => {
-    let isoCodes = await getIsoCodes();
-    let data = await getLastDataCountries();
-    isoCodes = isoCodes.map((obj) => {
-      let countryData = data.find(country => country.countryId === obj.countryId);
-      if (!countryData) {
-        //Si no lo encuentra, buscamos por el code
-        countryData = data.find(country => country.countryId === missingCountriesCode[obj.code]);
-      }
+  useEffect(() => {
 
-      let fullVaccinatedPerHundred = countryData ? parseFloat(countryData.people_fully_vaccinated_per_hundred) : null;
-      return { country: obj.code, countryId: obj.countryId, value: fullVaccinatedPerHundred };
-    });
-    setData(isoCodes)
-    console.log(isoCodes.filter(iso => iso.value == null))
+
+    async function setDataAux() {
+      const missingCountriesCode = {
+        "BO": "Bolivia",
+        "BQ": "Bonaire Sint Eustatius and Saba",
+        "BN": "Brunei",
+        "CV": "Cape Verde",
+        "CI": "Cote d'Ivoire",
+        "CW": "Curacao",
+        "CD": "Democratic Republic of Congo",
+        "GB": "England",
+        "FK": "Falkland Islands",
+        "IR": "Iran",
+        "LA": "Laos",
+        "MD": "Moldova",
+        "CY": "Northern Cyprus",
+        "GB-NI": "Northern Ireland",
+        "PS": "Palestine",
+        "RU": "Russia",
+        "SH": "Saint Helena",
+        "KR": "South Korea",
+        "SY": "Syria",
+        "TW": "Taiwan",
+        "TL": "Timor",
+        "US": "United States",
+        "VE": "Venezuela",
+        "VN": "Vietnam",
+        "GB-WLS": "Wales",
+      }
+      let isoCodes = await getIsoCodes();
+      let data = await getLastDataCountries();
+      isoCodes = isoCodes.map((obj) => {
+        let countryData = data.find(country => country.countryId === obj.countryId);
+        if (!countryData) {
+          //Si no lo encuentra, buscamos por el code
+          countryData = data.find(country => country.countryId === missingCountriesCode[obj.code]);
+        }
+
+        let fullVaccinatedPerHundred = countryData ? parseFloat(countryData.people_fully_vaccinated_per_hundred) : null;
+        return { country: obj.code, countryId: obj.countryId, value: fullVaccinatedPerHundred };
+      });
+      setData(isoCodes)
+    }
+    setDataAux();
+
   }, []);
 
   const clickAction = (event, countryName) => {
