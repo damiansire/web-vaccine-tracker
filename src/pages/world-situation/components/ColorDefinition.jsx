e05cd4652b544acdb7afb4ea58d19c097b290930
Damian Sire <32693792+damiansire@users.noreply.github.com>
2021-07-22 20:28:58 -0300

    Add click in map

--- src/pages/world-situation/components/ColorDefinition.jsx
+++ src/pages/world-situation/components/ColorDefinition.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ColorDefinition = () => {
     return (
         <div>
-            <table class="table-auto">
+            <table className="table-auto">
                 <thead>
                     <tr>
                         <th>Color</th>
@@ -17,24 +17,24 @@ const ColorDefinition = () => {
                     </tr>
                     <tr>
                         <td className="bg-yellow-300"> Amarillo </td>
-                        <td>   Si los vacunados con todas las dosis son  mas del 50% </td> </tr>
-
+                        <td>   Si los vacunados con todas las dosis son  mas del 50% </td>
+                    </tr>
                     <tr>
                         <td className="bg-yellow-600"> Naranja </td>
-                        <td>  Si los vacunados con todas las dosis son  mas del 35% </td> </tr>
-
+                        <td>  Si los vacunados con todas las dosis son  mas del 35% </td>
+                    </tr>
                     <tr>
                         <td className="bg-red-600"> Rojo </td>
-                        <td>  Si los vacunados con todas las dosis son mas del 15%  </td> </tr>
-
+                        <td>  Si los vacunados con todas las dosis son mas del 15%  </td>
+                    </tr>
                     <tr>
                         <td className="bg-black text-white"> Negro </td>
-                        <td>  Si los vacunados con todas las dosis son menos del 15%  </td> </tr>
-
+                        <td>  Si los vacunados con todas las dosis son menos del 15%  </td>
+                    </tr>
                     <tr>
                         <td className="bg-blue-600"> Azul </td>
-                        <td> Tenemos un problema con los datos, entonces no lo estamos mostrando todavia. </td> </tr>
-
+                        <td> Tenemos un problema con los datos, entonces no lo estamos mostrando todavia. </td>
+                    </tr>
                 </tbody>
             </table>
 
