f54f57d38a20c849031bb7f3974b4338ba143943
Damian Sire <32693792+damiansire@users.noreply.github.com>
2021-09-03 22:12:44 -0300

    Feat: Commit change

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import styled from "styled-components"
+
 import "./App.css";
 import CountriesData from "./pages/countries-compare/pages/CountriesDataCompare";
 import AllCountriesRanking from "./pages/ranking/pages/AllCountriesRanking";
@@ -10,6 +12,16 @@ import WorldSituation from "./pages/world-situation/pages/WorldSituation"
 import InformationSources from "./pages/information-sources/pages/InformationSources";
 import ReactGA from "react-ga";
 import React, { useEffect } from "react";
+
+//El nombre de la variable, empiezan con style
+const StyleHomePageContainer = styled.div`
+  display: grid;
+  grid-template-columns: 15vw 1fr;
+  .right-container {
+    background: #f4f4fa;
+  }
+`
+
 
 function usePageViews() {
   let location = useLocation();
@@ -26,18 +38,22 @@ function App() {
   usePageViews();
   return (
     <div className="max-h-screen">
-      <Navbar />
 
-      <div>
-        <Switch>
-          <Route path="/" component={CountriesData} />
-          <Route path="/country/:countryId" component={CountryStatistics} />
-          <Route path="/ranking" component={AllCountriesRanking} />
-          <Route path="/all-rankings" component={AllRankings} />
-          <Route path="/world-situation" component={WorldSituation} />
-          <Route path="/information-source" component={InformationSources} />
-        </Switch>
-      </div>
+      <StyleHomePageContainer>
+
+        <Navbar />
+
+        <div className="right-container">
+          <Switch>
+            <Route path="/" component={CountriesData} />
+            <Route path="/country/:countryId" component={CountryStatistics} />
+            <Route path="/ranking" component={AllCountriesRanking} />
+            <Route path="/all-rankings" component={AllRankings} />
+            <Route path="/world-situation" component={WorldSituation} />
+            <Route path="/information-source" component={InformationSources} />
+          </Switch>
+        </div>
+      </StyleHomePageContainer>
     </div>
   );
 }
