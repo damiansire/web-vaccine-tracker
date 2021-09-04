989cf1ad5056e32b9da0fb3740f993634ec2fa10
Francisco Cobas <fcarocena@gmail.com>
2021-09-03 22:57:27 -0300

    Cambios en la UI de la home

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import styled from "styled-components"
+import styled from 'styled-components';
 
-import "./App.css";
-import CountriesData from "./pages/countries-compare/pages/CountriesDataCompare";
-import AllCountriesRanking from "./pages/ranking/pages/AllCountriesRanking";
-import CountryStatistics from "./pages/country-statistics/pages/CountryStatistics";
+import './App.css';
+import CountriesData from './pages/countries-compare/pages/CountriesDataCompare';
+import AllCountriesRanking from './pages/ranking/pages/AllCountriesRanking';
+import CountryStatistics from './pages/country-statistics/pages/CountryStatistics';
 
-import Navbar from "./components/Navbar.jsx";
-import { Route, Switch, useLocation } from "wouter";
-import AllRankings from "./pages/ranking/pages/AllRankings";
-import WorldSituation from "./pages/world-situation/pages/WorldSituation"
-import InformationSources from "./pages/information-sources/pages/InformationSources";
-import ReactGA from "react-ga";
-import React, { useEffect } from "react";
+import Navbar from './components/Navbar.jsx';
+import { Route, Switch, useLocation } from 'wouter';
+import AllRankings from './pages/ranking/pages/AllRankings';
+import WorldSituation from './pages/world-situation/pages/WorldSituation';
+import InformationSources from './pages/information-sources/pages/InformationSources';
+import ReactGA from 'react-ga';
+import React, { useEffect } from 'react';
 
 //El nombre de la variable, empiezan con style
 const StyleHomePageContainer = styled.div`
@@ -20,14 +20,13 @@ const StyleHomePageContainer = styled.div`
   .right-container {
     background: #f4f4fa;
   }
-`
-
+`;
 
 function usePageViews() {
   let location = useLocation();
   useEffect(() => {
-    ReactGA.initialize("UA-155224346-6", {
-      debug: true
+    ReactGA.initialize('UA-155224346-6', {
+      debug: true,
     });
     ReactGA.set({ page: location[0] });
     ReactGA.pageview(location[0]);
@@ -37,20 +36,18 @@ function usePageViews() {
 function App() {
   usePageViews();
   return (
-    <div className="max-h-screen">
-
+    <div className='max-h-screen'>
       <StyleHomePageContainer>
-
         <Navbar />
 
-        <div className="right-container">
+        <div className='right-container'>
           <Switch>
-            <Route path="/" component={CountriesData} />
-            <Route path="/country/:countryId" component={CountryStatistics} />
-            <Route path="/ranking" component={AllCountriesRanking} />
-            <Route path="/all-rankings" component={AllRankings} />
-            <Route path="/world-situation" component={WorldSituation} />
-            <Route path="/information-source" component={InformationSources} />
+            <Route path='/' component={CountriesData} />
+            <Route path='/country/:countryId' component={CountryStatistics} />
+            <Route path='/ranking' component={AllCountriesRanking} />
+            <Route path='/all-rankings' component={AllRankings} />
+            <Route path='/world-situation' component={WorldSituation} />
+            <Route path='/information-source' component={InformationSources} />
           </Switch>
         </div>
       </StyleHomePageContainer>
