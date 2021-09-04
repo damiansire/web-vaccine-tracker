c4a6b8a9c25ffb9785f65aa7133fc700151ee87c
Francisco Cobas <fcarocena@gmail.com>
2021-09-03 22:57:27 -0300

    Cambios en la UI de la home

--- src/pages/countries-compare/pages/CountriesDataCompare.jsx
+++ src/pages/countries-compare/pages/CountriesDataCompare.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
-import OptionsSelected from "../components/SelectCountryComponent/OptionsSelected";
+import OptionsSelected from '../components/SelectCountryComponent/OptionsSelected';
 
 import {
   FormControl,
@@ -9,76 +9,123 @@ import {
   InputLabel,
   Select,
   Checkbox,
-} from "@material-ui/core";
+} from '@material-ui/core';
 
 import {
   getAvailablesCountries,
   getCountryData,
-} from "../../../adapters/countries.js";
+} from '../../../adapters/countries.js';
 
 //Components
-import CountriesGraphs from "../components/CountriesGraphs";
-import CompareCountriesTable from "../components/CompareCountriesTable";
-import DoubleButton from "../components/DoubleButton";
-import SelectCountry from "../components/SelectCountryComponent/SelectCountry";
-import TodayDataTable from "../components/TodayDataTable";
-const { sortDateAsc } = require("../../../utils/sorts");
-const { normalizeCountries } = require("../../../utils/missingDate");
+import CountriesGraphs from '../components/CountriesGraphs';
+import CompareCountriesTable from '../components/CompareCountriesTable';
+import DoubleButton from '../components/DoubleButton';
+import SelectCountry from '../components/SelectCountryComponent/SelectCountry';
+import TodayDataTable from '../components/TodayDataTable';
+const { sortDateAsc } = require('../../../utils/sorts');
+const { normalizeCountries } = require('../../../utils/missingDate');
 
 const StyledHomePageContainer = styled.div`
+  margin: 2rem;
+  .header {
+    display: flex;
+    justify-content: space-between;
+    margin: 2rem 0;
+  }
   h1 {
     font-size: 35px;
     font-family: 'Roboto';
+    font-weight: bold;
   }
-
-`
-
+  h2 {
+    color: grey;
+  }
+  .select-data-container,
+  .option-selected-countries-container,
+  .graph-container {
+    background: white;
+    border-radius: 15px;
+    box-shadow: 0 0 15px #8e8e8e79;
+    padding: 1rem;
+  }
+  .select-data-container p {
+    color: grey;
+  }
+  .countries-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .selected-countries-container,
+  .graph-and-title-container {
+    margin-top: 2rem;
+  }
+  .selected-countries-container p,
+  .graph-and-title-container p {
+    color: grey;
+    margin-bottom: 0.5rem;
+  }
+  #available-countries {
+    border: none;
+    border-bottom: 1px solid black;
+  }
+  .search-input-container {
+    position: relative;
+  }
+  .search-input-container svg {
+    position: absolute;
+    left: 0;
+    bottom: 15px;
+  }
+  .search-input-container input {
+    padding-left: 40px;
+  }
+`;
 
 const CountriesData = () => {
-
   const [loadCountryData, setLoadCountryData] = useState(false);
   const [selectedCountriesData, setCountriesDataState] = useState({});
-  const [viewInfo, setViewInfo] = useState("Graph");
+  const [viewInfo, setViewInfo] = useState('Graph');
   const [availablesCountries, setAvailablesCountries] = useState([]);
   const [sameOrigin, setSameOrigin] = useState(false);
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState('');
   const [selectedProperty, setSelectedProperty] = useState(
-    "people_vaccinated_per_hundred"
+    'people_vaccinated_per_hundred'
   );
   const [selectedCountries, setSelectedCountries] = useState([
-    "Argentina",
-    "Uruguay",
-    "Chile",
-    "Colombia",
+    'Argentina',
+    'Uruguay',
+    'Chile',
+    'Colombia',
   ]);
 
   const countryAttributeNames = [
     {
-      key: "people_vaccinated_per_hundred",
-      text: "% Poblacion vacunada",
+      key: 'people_vaccinated_per_hundred',
+      text: '% Poblacion vacunada',
     },
     {
-      key: "people_fully_vaccinated_per_hundred",
-      text: "% Poblacion Inmunizada",
+      key: 'people_fully_vaccinated_per_hundred',
+      text: '% Poblacion Inmunizada',
     },
     {
-      key: "daily_vaccinations",
-      text: "Vacunacion diaria",
+      key: 'daily_vaccinations',
+      text: 'Vacunacion diaria',
     },
     {
-      key: "daily_vaccinations_per_million",
-      text: "Vacunacion diaria por millon",
+      key: 'daily_vaccinations_per_million',
+      text: 'Vacunacion diaria por millon',
     },
     {
-      key: "people_fully_vaccinated",
-      text: "Personas full vacunas",
+      key: 'people_fully_vaccinated',
+      text: 'Personas full vacunas',
     },
-    { key: "people_vaccinated", text: "Vacunadas" },
+    { key: 'people_vaccinated', text: 'Vacunadas' },
     {
-      key: "total_dose_vaccinations",
-      text: "Total de dosis aplicadas",
+      key: 'total_dose_vaccinations',
+      text: 'Total de dosis aplicadas',
     },
-    { key: "vaccine_type", text: "Tipo de vacuna" },
+    { key: 'vaccine_type', text: 'Tipo de vacuna' },
   ];
 
   useEffect(() => {
@@ -89,29 +136,29 @@ const CountriesData = () => {
 
   const countryAttributeDescripcion = {
     people_fully_vaccinated_per_hundred:
-      "Porcentaje de la población que ha recibido todas las dosis de las vacunas.",
+      'Porcentaje de la población que ha recibido todas las dosis de las vacunas.',
     people_vaccinated_per_hundred:
-      "Porcentaje de la poblacion con al menos una dosis",
-    daily_vaccinations: "Cantidad total de dosis administradas el día de hoy.",
+      'Porcentaje de la poblacion con al menos una dosis',
+    daily_vaccinations: 'Cantidad total de dosis administradas el día de hoy.',
     daily_vaccinations_per_million:
-      "Cantidad total de dosis aplicadas el dia de hoy por millon de habitantes",
+      'Cantidad total de dosis aplicadas el dia de hoy por millon de habitantes',
     people_fully_vaccinated:
-      "Cantidad de personas que han recibido todas las dosis de las vacunas.",
-    people_vaccinated: "Cantidad de personas vacunadas con al menos una dosis.",
+      'Cantidad de personas que han recibido todas las dosis de las vacunas.',
+    people_vaccinated: 'Cantidad de personas vacunadas con al menos una dosis.',
     total_dose_vaccinations:
-      "Total de dosis aplicadas. Si una persona se da 2 dosis, se cuenta dos veces.",
-    vaccine_type: "Tipo de vacuna",
+      'Total de dosis aplicadas. Si una persona se da 2 dosis, se cuenta dos veces.',
+    vaccine_type: 'Tipo de vacuna',
   };
 
   const countryAttributeNamesTranslate = {
-    people_vaccinated_per_hundred: "% Poblacion vacunada",
-    people_fully_vaccinated_per_hundred: "% Poblacion Inmunizada",
-    daily_vaccinations: "Vacunacion diaria",
-    daily_vaccinations_per_million: "Vacunacion diaria por millon",
-    people_fully_vaccinated: "Personas full vacunas",
-    people_vaccinated: "Vacunadas",
-    total_dose_vaccinations: "Total de dosis aplicadas",
-    vaccine_type: "Tipo de vacuna",
+    people_vaccinated_per_hundred: '% Poblacion vacunada',
+    people_fully_vaccinated_per_hundred: '% Poblacion Inmunizada',
+    daily_vaccinations: 'Vacunacion diaria',
+    daily_vaccinations_per_million: 'Vacunacion diaria por millon',
+    people_fully_vaccinated: 'Personas full vacunas',
+    people_vaccinated: 'Vacunadas',
+    total_dose_vaccinations: 'Total de dosis aplicadas',
+    vaccine_type: 'Tipo de vacuna',
   };
 
   useEffect(() => {
@@ -138,21 +185,21 @@ const CountriesData = () => {
           })
         );
         contriesData.forEach((countryData) => {
-          countryData["data"] = countryData["data"].sort((a, b) =>
+          countryData['data'] = countryData['data'].sort((a, b) =>
             sortDateAsc(a, b)
           );
 
           saveCountryDataInCached(
-            countryData["countryName"],
-            countryData["data"]
+            countryData['countryName'],
+            countryData['data']
           );
         });
       }
 
       let countriesDataFromCached = selectedCountries.map((countryName) => {
         return {
-          name: countriesDataCached[countryName]["countryName"],
-          data: countriesDataCached[countryName]["data"],
+          name: countriesDataCached[countryName]['countryName'],
+          data: countriesDataCached[countryName]['data'],
         };
       });
 
@@ -193,162 +240,121 @@ const CountriesData = () => {
     }
   };
 
-
-
   return (
     <StyledHomePageContainer>
       {loadCountryData && (
-        <div className="">
-
-          <h1>Comparar evolución de Paises</h1>
-
-          <div id="quote">
-            <p className="italic text-gray-600">
-              {countryAttributeDescripcion[selectedProperty]}
-            </p>
+        <div className=''>
+          <div className='header'>
+            <div>
+              <h1>Comparar evolución de Paises</h1>
+              <h2>{countryAttributeDescripcion[selectedProperty]}</h2>
+            </div>
+            <DoubleButton
+              buttonsHandle={selectViewData}
+              button1Text='Grafico'
+              valueButton1='Graph'
+              button2Text='Tabla'
+              valueButton2='Table'
+              selectedOption={viewInfo}
+            />
           </div>
-          <div>
-            <Select
-              variant="outlined"
-              native
-              onChange={(event) => {
-                setSelectedProperty(event.target.value);
-              }}
-            >
-              {countryAttributeNames.map((dataOption) => (
-                <option value={dataOption.key} key={dataOption.key}>
-                  {dataOption.text}
-                </option>
-              ))}
-            </Select>
-          </div>
-
-          <div className="col-span-12 sm:col-span-10 w-full justify-items-center gap-4 align-middle">
-            <div className="grid grid-cols-12 m-2">
+          <div className='select-data-container'>
+            <div className='countries-container'>
               <div>
-                <div className="col-start-4 col-span-7 text-center text-3xl self-center">
-                  GRAFICO DE EVOLUCIÓN
-                </div>
+                <p>Dato a ver</p>
+                <Select
+                  variant='outlined'
+                  native
+                  onChange={(event) => {
+                    setSelectedProperty(event.target.value);
+                  }}
+                >
+                  {countryAttributeNames.map((dataOption) => (
+                    <option value={dataOption.key} key={dataOption.key}>
+                      {dataOption.text}
+                    </option>
+                  ))}
+                </Select>
               </div>
-              <div className="text-center col-span-12 sm:col-span-1">
-                <DoubleButton
-                  buttonsHandle={selectViewData}
-                  button1Text="Grafico"
-                  valueButton1="Graph"
-                  button2Text="Tabla"
-                  valueButton2="Table"
-                  selectedOption={viewInfo}
-                />
-              </div>
-            </div>
-            <div className="grid grid-cols-12">
-              <div className="col-span-12 sm:col-span-2 flex flex-col justify-items-center gap-4 align-middle p-4">
-                <div className="col-span-12 sm:col-start-2 sm:col-span-1 my-1">
-                  <FormControl>
-                    <InputLabel htmlFor="age-native-simple">
-                      Dato a ver
-                    </InputLabel>
-                  </FormControl>
-                </div>
+              <div className='search-input-container'>
+                <p>Buscar pais...</p>
 
-                <div className="col-span-12 sm:col-span-1 bg-gray-200  justify-center items-center">
-                  <div className="max-w-sm bg-white border-2 border-gray-300 p-6 rounded-md tracking-wide shadow-lg">
-                    <div
-                      id="header"
-                      className="flex items-center place-content-center mb-4"
-                    >
-                      <div id="header-text" className="leading-5  sm">
-                        <h4 id="name" className="text-lg font-semibold text-center">
-                          {countryAttributeNamesTranslate[selectedProperty]}
-                        </h4>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="col-span-1 my-1 text-center">
-                  <FormControlLabel
-                    control={
-                      <Checkbox
-                        checked={sameOrigin}
-                        onChange={() => {
-                          setSameOrigin(!sameOrigin);
-                        }}
-                        name="checkedB"
-                        color="primary"
-                      />
-                    }
-                    label="Mover graficas al 0"
-                  />
-                </div>
-                <span className="text-center font-medium">
-                  Paises seleccionados
-                </span>
-                <OptionsSelected
-                  selectedCountries={selectedCountries}
-                  setSelectedCountries={setSelectedCountries}
-                />
-              </div>
-              <div className="col-span-12 sm:col-span-10">
-                {/* Add key={selectedCountriesData} beucase the graph have a problem with refersh when remove country */}
-                {viewInfo === "Graph" && (
-                  <CountriesGraphs
-                    key={selectedCountriesData}
-                    optionsSelectedData={selectedProperty}
-                    countriesData={selectedCountriesData}
-                  />
-                )}
-                {viewInfo === "Graph" && (
-                  <div className="text-center font-medium my-3">
-                    Comparación de la situación de paises
-                  </div>
-                )}
-                {viewInfo === "Graph" && (
-                  <TodayDataTable countriesData={selectedCountriesData} />
-                )}
-                {viewInfo === "Table" && (
-                  <CompareCountriesTable
-                    countriesData={selectedCountriesData}
-                    optionsSelectedData={selectedProperty}
-                  />
-                )}
-              </div>
-            </div>
-          </div>
-
-          <div className="col-span-12 sm:col-span-2 ">
-            <div className="relative my-2">
-              <input
-                type="search"
-                className="bg-purple-white shadow rounded border-1 p-3 focus:bg-white focus:border-blue-400"
-                placeholder="Buscar pais..."
-                onChange={(event) => {
-                  setSearchTerm(event.target.value);
-                }}
-              />
-              <svg
-                version="1.1"
-                className="h-4 text-dark inline ml-2"
-                xmlns="http://www.w3.org/2000/svg"
-                x="0px"
-                y="0px"
-                viewBox="0 0 52.966 52.966"
-              >
-                <path
-                  d="M51.704,51.273L36.845,35.82c3.79-3.801,6.138-9.041,6.138-14.82c0-11.58-9.42-21-21-21s-21,9.42-21,21s9.42,21,21,21
+                <svg
+                  version='1.1'
+                  className='h-4 text-dark inline ml-2'
+                  xmlns='http://www.w3.org/2000/svg'
+                  x='0px'
+                  y='0px'
+                  viewBox='0 0 52.966 52.966'
+                >
+                  <path
+                    d='M51.704,51.273L36.845,35.82c3.79-3.801,6.138-9.041,6.138-14.82c0-11.58-9.42-21-21-21s-21,9.42-21,21s9.42,21,21,21
         c5.083,0,9.748-1.817,13.384-4.832l14.895,15.491c0.196,0.205,0.458,0.307,0.721,0.307c0.25,0,0.499-0.093,0.693-0.279
         C52.074,52.304,52.086,51.671,51.704,51.273z M21.983,40c-10.477,0-19-8.523-19-19s8.523-19,19-19s19,8.523,19,19
-        S32.459,40,21.983,40z"
+        S32.459,40,21.983,40z'
+                  />
+                </svg>
+                <input
+                  id='available-countries'
+                  list='availables-countries'
+                  type='text'
                 />
-              </svg>
+                <datalist id='availables-countries'>
+                  <option value='Chrome' />
+                  {availablesCountries.map((countryName) => (
+                    <option key={countryName} value={countryName} />
+                  ))}
+                </datalist>
+              </div>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={sameOrigin}
+                    onChange={() => {
+                      setSameOrigin(!sameOrigin);
+                    }}
+                    name='checkedB'
+                    color='primary'
+                  />
+                }
+                label='Mover graficas al 0'
+              />
             </div>
+          </div>
+          <div className='selected-countries-container'>
+            <p>Paises seleccionados</p>
+            <div className='option-selected-countries-container'>
+              <OptionsSelected
+                selectedCountries={selectedCountries}
+                setSelectedCountries={setSelectedCountries}
+              />
+            </div>
+          </div>
 
-            <SelectCountry
-              availablesCountries={availablesCountries}
-              selectedCountries={selectedCountries}
-              setSelectedCountries={setSelectedCountries}
-              searchTerm={searchTerm}
-            />
+          <div className='graph-and-title-container'>
+            <p>Comparación de la situación de los paises</p>
+            <div className='graph-container'>
+              {/* Add key={selectedCountriesData} beucase the graph have a problem with refersh when remove country */}
+              {viewInfo === 'Graph' && (
+                <CountriesGraphs
+                  key={selectedCountriesData}
+                  optionsSelectedData={selectedProperty}
+                  countriesData={selectedCountriesData}
+                />
+              )}
+              {viewInfo === 'Graph' && (
+                <div className=''>Comparación de la situación de paises</div>
+              )}
+              {viewInfo === 'Graph' && (
+                <TodayDataTable countriesData={selectedCountriesData} />
+              )}
+              {viewInfo === 'Table' && (
+                <CompareCountriesTable
+                  countriesData={selectedCountriesData}
+                  optionsSelectedData={selectedProperty}
+                />
+              )}
+            </div>
           </div>
         </div>
       )}
