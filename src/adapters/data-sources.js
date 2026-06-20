e168e058f52bb4d39ab40b52a17ecf4c4d588969
Damian Sire <damiansire@gmail.com>
2026-06-20 11:53:19 -0500

    fix(debugger-en-adapter): quita debugger de getSourcesForCountry [manual]

--- src/adapters/data-sources.js
+++ src/adapters/data-sources.js
@@ -1,7 +1,6 @@
 const apiEndpoint = process.env.REACT_APP_VACCINATION_API_ENDPOINT;
 
 export const getSourcesForCountry = async (countryId) => {
-    debugger;
     let response = await fetch(`${apiEndpoint}/sources-data/countries/${countryId}`);
     let data = await response.json();
     return data.sources;
